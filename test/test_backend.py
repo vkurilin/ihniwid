eb961c69204d8fc19f61d0d3f41032bbc5ec7ef8
vkurilin <vkurilin@smartdec.ru>
2021-04-13 17:24:55 +0300

    nocapthca test

--- test/test_backend.py
+++ test/test_backend.py
@@ -1,5 +1,26 @@
+import requests
+
 from ihniwid.backend import foo
 
 
-def test_foo() -> None:
-    assert 1 == 0 ** 0
+def test_nocaptcha(requests_mock, capsys):
+    def custom_matcher(request):
+        if (
+            request.url
+            == "https://stackoverflow.com/search?q=undefined+is+not+a+function"
+        ):
+            resp = requests.Response()
+            resp.status_code = 200
+            resp.url = "https://stackoverflow.com/nocaptcha?s=a9596900-03df-4660-88f5-03949ed99dad"
+            return resp
+        return None
+
+    requests_mock._adapter.add_matcher(custom_matcher)
+
+    foo("undefined is not a function")
+
+    captured = capsys.readouterr()
+    assert (
+        "Oh noes! Please solve this captcha: https://stackoverflow.com/nocaptcha?s=a9596900-03df-4660-88f5-03949ed99dad"
+        in captured.err
+    )
